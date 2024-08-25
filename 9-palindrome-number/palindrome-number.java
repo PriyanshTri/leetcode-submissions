7f2a7dbc63a96a792a341b64d5efaaeb3733dc48
Priyansh Tripathi <71260531+PriyanshTri@users.noreply.github.com>
2024-08-25 13:02:11 +0530

    Time: 4 ms (100.00%) | Memory: 43.9 MB (61.62%) - LeetSync

--- 9-palindrome-number/palindrome-number.java
+++ 9-palindrome-number/palindrome-number.java
@@ -1,31 +1,19 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        if (x < 0 || (x % 10 == 0 && x != 0))
+         if (x < 0 || (x % 10 == 0 && x != 0)) {// if negative or number ends with 0, it can't be palindrome integer.
             return false;
-        if (x >= 0 && x < 10)
-            return true;
-        
-        int countOfDigits = 0;
-        int copyNum = x;
-        while (copyNum != 0) {
-            countOfDigits++;
-            copyNum /= 10;
         }
         
-        copyNum = x;
-        int divisor = (int) Math.pow(10, countOfDigits - 1);
+        int initially = x;
+        int reversed = 0;
+
         
-        while (copyNum != 0) {
-            int lastDigit = copyNum % 10;
-            int firstDigit = copyNum / divisor;
-            
-            if (firstDigit != lastDigit)
-                return false;
-            
-            copyNum = (copyNum % divisor) / 10;
-            divisor /= 100;
+        while (x > reversed) {//reversing till middle(if number contains even digits) else middle+1;
+            reversed = reversed * 10 + x % 10;
+            x /= 10;
         }
-        
-        return true;
+
+        //if the number contained odd digit, the condition "x == reversed/10 " will remove the middle digit and will make it contain even digits and hence if equal then true otherwise false will be returned.
+        return x == reversed || x == reversed / 10;
     }
 }
