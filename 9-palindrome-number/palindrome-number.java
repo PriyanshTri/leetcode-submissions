36b7fd2129388395630e4ff652db43bd181da1bc
Priyansh Tripathi <71260531+PriyanshTri@users.noreply.github.com>
2024-08-25 13:00:10 +0530

    Time: 5 ms (69.81%) | Memory: 43.8 MB (61.62%) - LeetSync

--- 9-palindrome-number/palindrome-number.java
+++ 9-palindrome-number/palindrome-number.java
@@ -1,13 +1,31 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        if(x<0) return false;
-        int newNum = 0;
+        if (x < 0 || (x % 10 == 0 && x != 0))
+            return false;
+        if (x >= 0 && x < 10)
+            return true;
+        
+        int countOfDigits = 0;
         int copyNum = x;
-        while(copyNum!=0) {
-            newNum*=10;
-            newNum+=copyNum%10;
-            copyNum/=10;
+        while (copyNum != 0) {
+            countOfDigits++;
+            copyNum /= 10;
         }
-        return newNum == x;
+        
+        copyNum = x;
+        int divisor = (int) Math.pow(10, countOfDigits - 1);
+        
+        while (copyNum != 0) {
+            int lastDigit = copyNum % 10;
+            int firstDigit = copyNum / divisor;
+            
+            if (firstDigit != lastDigit)
+                return false;
+            
+            copyNum = (copyNum % divisor) / 10;
+            divisor /= 100;
+        }
+        
+        return true;
     }
 }
