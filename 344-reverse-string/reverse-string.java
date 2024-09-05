597db4ff57b6d803f5d6776392789ded5f06af3a
Priyansh Tripathi <71260531+PriyanshTri@users.noreply.github.com>
2024-09-05 09:26:32 +0530

    Time: 1 ms (25.35%) | Memory: 46.2 MB (74.43%) - LeetSync

--- 344-reverse-string/reverse-string.java
+++ 344-reverse-string/reverse-string.java
@@ -1,13 +1,13 @@
 class Solution {
+    // lets try it with recursion
     public void reverseString(char[] s) {
-        int startPointer = 0;
-        int endPointer = s.length-1;
-        while(startPointer<endPointer) {
-            char tempValue = s[startPointer];
-            s[startPointer] = s[endPointer];
-            s[endPointer] = tempValue;
-            startPointer++;
-            endPointer--;
-        }
+        RecursiveReverseString(0, s.length-1, s);
+    }
+    public void RecursiveReverseString(int startPointer, int endPointer, char[] s) {
+        if(startPointer>=endPointer) return;
+        char temp = s[startPointer];
+        s[startPointer] = s[endPointer];
+        s[endPointer] = temp;
+        RecursiveReverseString(startPointer+1, endPointer-1, s);
     }
 }
