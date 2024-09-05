ebd768ab8f937766d23c632535fb0ee52c981ddd
Priyansh Tripathi <71260531+PriyanshTri@users.noreply.github.com>
2024-09-05 16:33:41 +0530

    Time: 2 ms (99.19%) | Memory: 42.8 MB (85.43%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,36 +1,18 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        // lets do it with two pointer-
-        int startPointer = 0;
-        int endPointer = s.length() - 1;
-        while (startPointer < endPointer) {
-            char startPointerChar = s.charAt(startPointer);
-            char endPointerChar = s.charAt(endPointer);
-            if(!checkIfAlphaNumeric(startPointerChar)){
-                startPointer++;
-            }
-            else if(!checkIfAlphaNumeric(endPointerChar)) {
-                endPointer--;
-            } else if (checkIfAlphaNumeric(startPointerChar) && checkIfAlphaNumeric(endPointerChar)) {
-                startPointerChar = Character.toLowerCase(startPointerChar);
-                endPointerChar = Character.toLowerCase(endPointerChar);
-                if (startPointerChar == endPointerChar) {
-                    startPointer++;
-                    endPointer--;
-                } else {
-                    return false;
-                }
+        int i = 0, j = s.length() - 1;
+        while (i < j) {
+            if (!Character.isLetterOrDigit(s.charAt(i))) {
+                ++i;
+            } else if (!Character.isLetterOrDigit(s.charAt(j))) {
+                --j;
+            } else if (Character.toLowerCase(s.charAt(i)) != Character.toLowerCase(s.charAt(j))) {
+                return false;
+            } else {
+                ++i;
+                --j;
             }
         }
         return true;
-    }
-
-    public boolean checkIfAlphaNumeric(char currentChar) {
-        if ((currentChar >= 'a' && currentChar <= 'z') || (currentChar >= 'A' && currentChar <= 'Z')
-                || currentChar >= '0' && currentChar <= '9') {
-            return true;
-        } else {
-            return false;
-        }
     }
 }
