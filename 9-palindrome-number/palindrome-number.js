0b0ad270b30bbb350470a48cb35a51e426ff1407
Priyansh Tripathi <71260531+PriyanshTri@users.noreply.github.com>
2026-05-12 11:38:34 +0530

    Time: 5 ms (78.45%) | Memory: 63.5 MB (71.20%) - LeetSync

--- 9-palindrome-number/palindrome-number.js
+++ 9-palindrome-number/palindrome-number.js
@@ -3,6 +3,17 @@
  * @return {boolean}
  */
 var isPalindrome = function(x) {
-    const y= x.toString();
-    return y == y.split("").reverse().join("");
+    const isXPositive = x>=0;
+    if (!isXPositive) {
+        return false;
+    }
+    let reverseOfNum =0;
+    let absOfx = Math.abs(x);
+    
+    while(absOfx>0){
+        reverseOfNum*=10;
+        reverseOfNum+=(absOfx%10);
+        absOfx = Math.floor(absOfx/10);
+    }
+    return reverseOfNum == x;
 };
