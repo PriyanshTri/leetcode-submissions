82f946120e09beb330b48a0ba1e5ccfe5ea40c2a
Priyansh Tripathi <71260531+PriyanshTri@users.noreply.github.com>
2026-05-18 13:24:58 +0530

    Time: 1 ms (40.91%) | Memory: 54 MB (66.82%) - LeetSync

---
+++ 2106-find-greatest-common-divisor-of-array/find-greatest-common-divisor-of-array.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findGCD = function(nums) {
+    let min = nums[0];
+    let max = nums[0];
+    let gcd = 1;
+    for(let i = 1;i<nums.length; i++) {
+        if (min > nums[i]) {
+            min = nums[i];
+        }
+        else if(max<nums[i]) {
+            max = nums[i];
+        }
+    }
+    if(min === 1 || max === 1 || min===max)
+        return min;
+    for(let i = 1 ; i<min+1;i++) {
+        if(min%i===0 && max%i===0) {
+            gcd = i;
+        }
+    }
+    return gcd
+};
